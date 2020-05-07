5926aa25077338ae2eac35dfee59c57d96350a0d
Richard Chan <richardchanme@gmail.com>
2020-05-06 23:01:25 -0700

    Finished range input without much styling

--- src/components/RangeInput/RangeInput.js
+++ src/components/RangeInput/RangeInput.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './RangeInput.module.css';
 
 const RangeInput = () => {
-  const [input, setInput] = useState(3);
+  const [input, setInput] = useState(6);
 
   const handleChange = e => {
     setInput(e.target.value)
@@ -10,15 +10,21 @@ const RangeInput = () => {
 
   return (
     <div className={styles.container}>
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value={input}
-        onChange={handleChange}
-        className={styles.range}
-        step="1"/>
-      <span>{input}</span>
+      <div className={styles.range}>
+        <span>0</span>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={input}
+          onChange={handleChange}
+          step="1"/>
+        <span>100</span>
+      </div>
+      <p>Current value: {input}</p>
+      <div>
+        <h6>Range Input</h6>
+      </div>
     </div>
   )
 }
